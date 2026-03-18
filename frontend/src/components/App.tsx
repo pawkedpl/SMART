981dfaba9855f3f959b15e9683c17abe93217ca5
Paweł Kędzierski <147321628+pawkedpl@users.noreply.github.com>
2026-03-18 04:00:39 +0100

    start3

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+function App() {
+  return (
+    <div>
+      <h1>SMART - File Review Platform</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
